4e929500af117b76c333fd5f13996732d4ecce0f
price tula <pricetula@gmail.com>
2020-10-18 15:25:10 +0300

    added js things directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import AppChildPost from './components/AppChildPost';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      hello world
-    </div>
-  );
+const details = [
+  {
+    id: 1,
+    name: 'Mango',
+    message: 'soskosk sijisu osiuoisuou osiusoiu'
+  },
+  {
+    id: 2,
+    name: 'Mpp o',
+    message: 's  sisisin [ps[psiuoisuou osiusoiu'
+  },
+  {
+    id: 3,
+    name: 'oragne',
+    message: 'sossss  osiusoiu'
+  },
+  {
+    id: 4,
+    name: 'tomato',
+    message: 'toooos osiusoiu'
+  }
+]
+
+class App extends React.Component {
+  click() {
+    console.log('Clicked');
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          details.map(
+            detail => (
+              <AppChildPost
+                key={detail.id}
+                id={detail.id}
+                name={detail.name}
+                message={detail.message}
+                clicked={this.click}
+              />
+            )
+          )
+        }
+      </ul>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AppChildPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class AppChildPost extends React.Component {
+    render() {
+        return (
+            <div>Child component</div>
+        )
+    }
+}
+
+export default AppChildPost;
