90721e4052f298fb7627146dfa34762fe8a0aac9
price tula <pricetula@gmail.com>
2020-10-18 15:12:26 +0300

    removed unnecessary stuff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css';
-import PostBox from './components/PostBox.js';
-import ExpandPost from'./components/ExpandPost.js';
-
-
 
 function App() {
   return (
     <div className="app">
-      <div className="appComponents">
-        <PostBox />
-       
-      </div>
+      hello world
     </div>
   );
 }
